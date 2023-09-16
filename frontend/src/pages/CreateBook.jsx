04a055395d33864ee0027603d975f954d5c02ef3
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-09-16 11:56:14 +0530

    ShowButton component done

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 
 function CreateBook() {
+
+  const [title, setTitle] = useState('');
+  const [author,setAuthor] = useState('')
+  const [publishYear , setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook = () => {
+    const data = {
+      title, 
+      author,
+      publishYear
+    }
+
+    axios
+    .post('http://localhost:4000/api/book')
+  }
+
+
   return (
     <div>
-      Craetetr
+      
     </div>
   )
 }
