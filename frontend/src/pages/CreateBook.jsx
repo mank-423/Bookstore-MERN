48ecc97a7cb00d31f3bc65dab0aa4dbef7ef7242
Mayank Kumar <96490105+mank-423@users.noreply.github.com>
2023-09-16 12:24:28 +0530

    CreatBook component done

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -8,26 +8,81 @@ import { useNavigate } from 'react-router-dom'
 function CreateBook() {
 
   const [title, setTitle] = useState('');
-  const [author,setAuthor] = useState('')
-  const [publishYear , setPublishYear] = useState('');
+  const [author, setAuthor] = useState('')
+  const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
 
-  const handleSaveBook = () => {
+  const onHandleSaveBook = () => {
     const data = {
-      title, 
+      title,
       author,
       publishYear
     }
 
     axios
-    .post('http://localhost:4000/api/book')
+      .post('http://localhost:4000/api/book', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please check console');
+        console.log(error);
+      })
   }
 
 
   return (
-    <div>
-      
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create Book</h1>
+      {
+        loading
+          ? <Spinner />
+          : ''
+      }
+
+      <div className='flex flex-col border-2 border-sly-400 rounded-xl w-[600px]'>
+        
+        <div className='my-4 px-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-lg'
+          />
+        </div>
+
+        <div className='my-4 px-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input 
+            type="text" 
+            value={author}
+            onChange={(e)=> setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-lg' 
+          />
+        </div>
+
+        <div className='my-4 px-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input 
+            type="number" 
+            value={publishYear}
+            onChange={(e)=> setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-lg' 
+          />
+        </div>
+
+        <button 
+          className='p-2 bg-sky-300 hover:bg-sky-500 m-8'
+          onClick={onHandleSaveBook}>
+          CREATE
+        </button>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, {useState} from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function EditBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('')
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const onHandleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear
+    }
+
+    axios
+      .post('http://localhost:4000/api/book', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please check console');
+        console.log(error);
+      })
+  }
+
+
   return (
-    <div>
-      Edit
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Edit Book</h1>
+      {
+        loading
+          ? <Spinner />
+          : ''
+      }
+
+      <div className='flex flex-col border-2 border-sly-400 rounded-xl w-[600px]'>
+        
+        <div className='my-4 px-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-lg'
+          />
+        </div>
+
+        <div className='my-4 px-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input 
+            type="text" 
+            value={author}
+            onChange={(e)=> setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-lg' 
+          />
+        </div>
+
+        <div className='my-4 px-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input 
+            type="number" 
+            value={publishYear}
+            onChange={(e)=> setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-lg' 
+          />
+        </div>
+
+        <button 
+          className='p-2 bg-sky-300 hover:bg-sky-500 m-8'
+          onClick={onHandleSaveBook}>
+          CREATE
+        </button>
+      </div>
     </div>
   )
 }
